11d5b26687382b2d3d37b97858b5d06cd4544cda
abgalphabetz <arthur.mw.yeung@gmail.com>
2019-11-10 15:37:57 +0800

    maker components update

--- src/makerAction.tsx
+++ src/makerAction.tsx
@@ -2,12 +2,6 @@ import { types } from './makerTypes'
 import { Maker } from './store'
 
 export const actions = {
-    newMakers(makers: Maker[]) {
-        return {
-            type: types.NEW_MAKERS,
-            payload: makers
-        };
-    },
     fetchMakers() {
         return {
             type: types.FETCH_MAKERS
--- src/makerTypes.tsx
+++ src/makerTypes.tsx
@@ -1,5 +1,4 @@
 export const types = {
-    NEW_MAKERS: 'NEW_MAKERS',
     FETCH_MAKERS: 'FETCH_MAKERS',
     FETCH_MAKERS_SUCCESS: 'FETCH_MAKERS_SUCCESS'
 };
